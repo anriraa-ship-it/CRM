08f763dbad21c0dccafc4d39f193da994d3e8179
anriraa-ship-it <anriraa@gmail.com>
2026-05-30 19:56:28 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'designflow-v16';
+const ASSETS = ['./', './index.html'];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate' || e.request.url.includes('index.html')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
